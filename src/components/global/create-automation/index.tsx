ca12473d18d69b441b6590011b13fca98a3fe245
thestarharsh <talatiharsh19@gmail.com>
2025-01-26 00:40:37 +0530

    Enhance AutomationPage layout with automation list and CreateAutomation component

--- src/components/global/create-automation/index.tsx
+++ src/components/global/create-automation/index.tsx
@@ -7,7 +7,6 @@ const CreateAutomation = () => {
     return (
         <Button
             className="lg:px-10 py-6 bg-gradient-to-br hover:opacity-80 text-white rounded-full from-[#3352CC] font-medium to-[#1C2D70]"
-            onClick={() => {}}
         >
             <Loader state={false}>
                 <AutomationDuoToneWhite />
